dc0caa0d89b09a3243431f1502830b3bff42a197
Paul Oyinloye <oyinloyepma@gmail.com>
2025-11-24 19:03:46 +0000

    employee list creation

--- Backend/server.js
+++ Backend/server.js
@@ -2,3 +2,25 @@ import express from "express";
 import cors from "cors";
 import multer from "multer";
 import db from "./db.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static("../frontend"));
+
+const storage = multer.diskStorage({
+  destination: "uploads/",
+  filename: (_, file, cb) => cb(null, Date.now() + ".jpg")
+});
+
+const upload = multer({ storage });
+
+
+//employee list
+
+app.get("/employees", (req, res) => {
+  db.all("SELECT * FROM employees", [], (err, rows) => {
+    if (err) return res.status(500).json({ error: err });
+    res.json(rows);
+  });
+});
