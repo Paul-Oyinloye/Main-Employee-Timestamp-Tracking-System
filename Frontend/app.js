71ccf6873d4c86bc1690cd437bd2cc62f3be24bc
Paul Oyinloye <oyinloyepma@gmail.com>
2025-12-02 17:29:12 +0000

    add video and stream references to App component

--- Frontend/app.js
+++ Frontend/app.js
@@ -3,4 +3,5 @@ const { useState, useEffect, useRef } = React;
 function App() {
   const [employees, setEmployees] = useState([]);
   const [selected, setSelected] = useState(null);
-  
+  const videoRef = useRef(null);
+  const streamRef = useRef(null);
