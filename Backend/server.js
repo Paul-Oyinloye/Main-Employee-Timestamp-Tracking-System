c92d29592e8a6eaab54d8c6b9834bc0be1dad136
Paul Oyinloye <oyinloyepma@gmail.com>
2025-12-09 16:00:19 +0000

    fix: remove explicit host binding in server listen method

--- Backend/server.js
+++ Backend/server.js
@@ -78,6 +78,6 @@ app.get("/timestamps/:id", (req, res) => {
 //to start server
 const PORT = 3001;
 
-app.listen(PORT, "127.0.0.1", () => {
+app.listen(PORT, () => {
   console.log(`Server running at http://127.0.0.1:${PORT}`);
 });
