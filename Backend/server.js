95c490740693e698023c839e7de81cbd92ed0708
Paul Oyinloye <oyinloyepma@gmail.com>
2025-12-09 15:44:59 +0000

    fix: update server listen address for consistency in logging binding

--- Backend/server.js
+++ Backend/server.js
@@ -78,6 +78,6 @@ app.get("/timestamps/:id", (req, res) => {
 //to start server
 const PORT = 3001;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "127.0.0.1", () => {
+  console.log(`Server running at http://127.0.0.1:${PORT}`);
 });
