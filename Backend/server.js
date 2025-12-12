25e8d6a0dcf80e87a1bfb6d035290178c609248f
Paul Oyinloye <oyinloyepma@gmail.com>
2025-12-12 12:06:09 +0000

    Refactor code structure for improved readability and maintainability

--- Backend/server.js
+++ Backend/server.js
@@ -144,7 +144,7 @@ app.get("/timestamps/:id", (req, res) => {
 });
 
 // ----- START SERVER -----
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(">>> app.listen callback FIRED <<<");
